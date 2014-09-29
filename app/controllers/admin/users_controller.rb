898315e3a231fd6f7ff3cab4d993f9e31f37efe3
sruthireddy12 <gsruthireddy1291@gmail.com>
2014-09-29 18:22:35 +0530

    style issues fixes

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,7 +4,7 @@ class Admin::UsersController < Admin::AdminController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = current_user.organization.users
   end
 
   def show
