520e0eaca82c78455f4ae1ab1da93d2bc661f951
sruthireddy12 <gsruthireddy1291@gmail.com>
2014-09-30 18:26:19 +0530

    validation added for project name and  other issues fixes

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,6 +5,8 @@ class Project < ActiveRecord::Base
   has_many :reports , through: :test_runs
   accepts_nested_attributes_for :pdf_xml_mappings, allow_destroy: true
 
-  validates_presence_of :organization_id,:name,:pdf_folder,:input_folder
+  validates_presence_of :organization_id, :name, :pdf_folder,:input_folder
+  validates_format_of :name, :with => /[\w \.\-@]+/,
+  :message => "can only contain letters and numbers."
   validates_uniqueness_of :name, scope: :organization_id
 end
