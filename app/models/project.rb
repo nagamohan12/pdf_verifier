33b40094fac56d783508e519d296a75c6dc83a6c
sruthireddy12 <gsruthireddy1291@gmail.com>
2014-10-08 15:59:18 +0530

    validation for date

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,7 +7,10 @@ class Project < ActiveRecord::Base
   #has_many :last_runs, -> { last_run_at }, class_name: 'TestRun'
  
   accepts_nested_attributes_for :pdf_xml_mappings, allow_destroy: true
-
+  # validates :next_run,
+  # date: { after: Proc.new { Date.today }, message: 'must be after today' },
+  # on: :create, on: :update
+  validates_date :next_run, :on => :create, :on => :update, :on_or_after => :today,  message: 'must be after today'
   validates_presence_of :organization_id, :name, :pdf_folder,:input_folder
   validates_format_of :name, :with => /[\w \.\-@]+/,
   :message => "can only contain letters and numbers."
@@ -22,5 +25,4 @@ class Project < ActiveRecord::Base
   	self.last_test_run.failed > 0 ? false : true
   end
 
-  
 end
